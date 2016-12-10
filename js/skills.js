04b3177c47588aa0ad14e530d45ce273f6e53596
Wouter Fennis <wouter_hilversum@hotmail.com>
2016-12-10 11:40:55 +0100

    ADDED initial files

---
+++ js/skills.js
@@ -0,0 +1,27 @@
+"use strict";
+jQuery(document).ready(function () {
+
+    console.log("Hey, toch even naar de source code kijken?");
+    console.log("Kijk anders op mijn Github voor meer code van mij");
+    console.log("Github link HIER");
+
+    $(window).scroll(function () {
+        var skillsTop = $('#skills').offset().top;
+        var skillsHeight = $('#skills').outerHeight();
+        var windowHeight = $(window).height();
+        var currentPosition = $(this).scrollTop();
+        
+        if (currentPosition > (skillsTop + skillsHeight - windowHeight)) {
+            animateSkills();
+        }
+    });
+
+});
+
+function animateSkills() {
+    jQuery('.skillbar').each(function () {
+        jQuery(this).find('.skillbar-bar').animate({
+            width: jQuery(this).attr('data-percent')
+        }, 6000);
+    });
+}
